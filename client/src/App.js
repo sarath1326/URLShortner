6e7aff03bc80f2ced630e5e92b5c7cc5d272fcbd
sarath <sarathsarath93366@gmail.com>
2023-11-03 12:41:53 +0530

    project first commit . client side and sever side ok to start coding .

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+      <h1> welcome </h1>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,16 @@
+
+ const express=require("express");
+ const app=express()
+
+
+
+
+
+ app.listen(3001,()=>{
+
+      
+       console.log("servre started")
+       
+ })
+
+ 
